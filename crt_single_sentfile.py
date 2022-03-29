c6070ca626805fe3ae149305def3749dce450121
CoSineZxc <2894635770@qq.com>
2022-03-29 18:16:25 +0100

    update based on new name of stimuli

--- crt_single_sentfile.py
+++ crt_single_sentfile.py
@@ -1,21 +1,21 @@
 import re
-name="prince"
+name="A"
 # num="1"
 
+for k in range(4):
+    for j in range(2):
+        num = str(j+1)
+        file_read = open("stimuli\\" + chr(ord("A")+k)+str(num) + ".txt", "r", encoding='gbk')
+        for i,line in enumerate(file_read):
+            line=re.sub(r"“","\"",line)
+            line = re.sub(r"”", "\"", line)
+            line = re.sub(r"’", "\'", line)
+            line = re.sub(r"…", ".", line)
+            file_write=open("rslt\\"+chr(ord("A")+k)+str(num)+f"{i+1:02}"+".txt","w")
+            file_write.writelines(line)
+            file_write.close()
 
-for j in range(2):
-    num = str(j+1)
-    file_read = open("stimuli\\" + num + "_" + name + ".txt", "r", encoding='gbk')
-    for i,line in enumerate(file_read):
-        line=re.sub(r"“","\"",line)
-        line = re.sub(r"”", "\"", line)
-        line = re.sub(r"’", "\'", line)
-        line = re.sub(r"…", ".", line)
-        file_write=open("rslt\\"+num+name+f"{i+1:02}"+".txt","w")
-        file_write.writelines(line)
-        file_write.close()
-
-    file_read.close()
+        file_read.close()
 
 
 # num="2"
--- timelength.py
+++ timelength.py
@@ -5,7 +5,7 @@ from scipy import stats
 import scipy.io as sio
 
 
-Path = 'D:\Project\Data\stringfile\\'
+Path = 'D:\Project\Data\stringfile\Exp2\\'
 
 def get_length(dataPath):
     with contextlib.closing(wave.open(dataPath,'r')) as f:
@@ -36,52 +36,56 @@ for i in range(4):
 
 
 # save .mat
-name = 'LengthSentence1.mat'
+name = 'Exp2LengthSentence1.mat'
 dic={}
-for i, name in enumerate(filename):
+for i, name in enumerate(namelist):
     dic[name]=timelist[i-1]
 sio.savemat(name, {'dict': dic})
 # sio.savemat(name, {'FileName': namelist, 'TimeLength': timelist})
 
 #
-# print("max: "+str(max(timelist)))
-# print("min: "+str(min(timelist)))
-# print("average: "+str(sum(timelist)/len(timelist)))
+print("max: "+str(max(timelist)))
+print("min: "+str(min(timelist)))
+print("average: "+str(sum(timelist)/len(timelist)))
 # mode=stats.mode(timelist)[0][0]
 # print("mode: "+str(mode))
 #
 #
 # plt.bar(range(len(timelist)), timelist)
 # plt.show()
-# timelist.sort()
-# plt.bar(range(len(timelist)), timelist)
-# plt.show()
-# n_1=0
-# n_2=0
-# n_3=0
-# n_4=0
-# n_5=0
-# n_6=0
-# n_7=0
-# for i in timelist:
-#     if i<2.7:
-#         n_1=n_1+1
-#     elif i >= 2.7 and i < 2.8:
-#         n_7 = n_7 + 1
-#     elif i>=2.8 and i<2.9:
-#         n_2=n_2+1
-#     elif i >= 2.9 and i < 3.0:
-#         n_3 = n_3 + 1
-#     elif i>=3.0 and i<3.1:
-#         n_4=n_4+1
-#     elif i>=3.1 and i<3.2:
-#         n_5=n_5+1
-#     else:
-#         n_6=n_6+1
-# print(n_1)
-# print(n_7)
-# print(n_2)
-# print(n_3)
-# print(n_4)
-# print(n_5)
-# print(n_6)
+timelist.sort()
+plt.bar(range(len(timelist)), timelist)
+plt.show()
+n_1=0
+n_2=0
+n_3=0
+n_4=0
+n_5=0
+n_6=0
+n_7=0
+for i in timelist:
+    if i<2.7:
+        n_1=n_1+1
+    elif i >= 2.7 and i < 2.8:
+        n_7 = n_7 + 1
+    elif i>=2.8 and i<2.9:
+        n_2=n_2+1
+    elif i >= 2.9 and i < 3.0:
+        n_3 = n_3 + 1
+    elif i>=3.0 and i<3.1:
+        n_4=n_4+1
+    elif i>=3.1 and i<3.2:
+        n_5=n_5+1
+    else:
+        n_6=n_6+1
+print("0-2.7s: ",str(n_1))
+print("2.7-2.8s: ",str(n_7))
+print("2.8-2.9s: ",str(n_2))
+print("2.9-3.0s: ",str(n_3))
+print("3.0-3.1s: ",str(n_4))
+print("3.1-3.2s: ",str(n_5))
+print("3.2-3.3s: ",str(n_6))
+N=[n_1,n_7,n_2,n_3,n_4,n_5,n_6]
+plt.xticks(range(7),['0-2.7','2.7-2.8','2.8-2.9','2.9-3.0','3.0-3.1','3.1-3.2','≥3.2'])
+plt.bar(range(len(N)), N)
+plt.show()
